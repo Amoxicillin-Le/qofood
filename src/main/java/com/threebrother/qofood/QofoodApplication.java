569ba33cbcd60bace68e85bb02ad60180fea0c22
ZLHash <631890498@qq.com>
2018-05-30 14:06:27 +0800

    Initial commit

---
+++ src/main/java/com/threebrother/qofood/QofoodApplication.java
@@ -0,0 +1,12 @@
+package com.threebrother.qofood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QofoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QofoodApplication.class, args);
+    }
+}
